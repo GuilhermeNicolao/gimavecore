21c61bd597d1d96ac4edc9ac6a0d290bd8b92034
Guilherme Nicolao <guilhermenicolaodasilva45@gmail.com>
2025-01-24 11:24:41 -0300

    Adicionado a pasta liberacoesTEDS

---
+++ GIMAVE/LiberacoesTEDs/script.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+# Acessando o site
+driver = webdriver.Chrome()
+driver.get("https://fomento.eucard.com.br/transferencias")
+
+
+#Manter o script em execução
+while True:
+    pass
+
+
