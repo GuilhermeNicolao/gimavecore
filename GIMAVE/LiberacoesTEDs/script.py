6cc128bd13a0d08d2f287ca70493d929ac5e58eb
Guilherme Nicolao <guilhermenicolaodasilva45@gmail.com>
2025-01-30 17:42:20 -0300

    Script finalizado, faltando duas comparacoes adicionais para maior seguranca

--- GIMAVE/LiberacoesTEDs/script.py
+++ GIMAVE/LiberacoesTEDs/script.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 import time
 
 load_dotenv()
@@ -51,7 +52,7 @@ time.sleep(1)
 transferencias_link = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//a[@href='/transferencias' and contains(@class, 'menu__item')]")))
 transferencias_link.click()
 
-time.sleep(10)
+time.sleep(20)
 
 
 
@@ -76,27 +77,84 @@ def escrever_celula(cell, value):
         print(f"Erro ao escrever na célula {cell}: {response.status_code} - {response.text}")
 
 
-row = 97
+#Célula inicial
+row = 2
 
-for i in range(3):
-
-    cell_a = f"A{row}"
-    value_a = ler_celula(cell_a)
-
-    try:
-        #CAPTURAR NOME PAINELFOMENTO
-        xpath_dinamico = f"//td[@class='text-left' and contains(text(), '{value_a}')]"
-        nome_td = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, xpath_dinamico)))
-        nome = nome_td.text
-
-        #pressionar_botao = nome_td.find_element(By.XPATH, "./preceding-sibling::td//div[@class='q-toggle__track']")
-        #pressionar_botao.click()
-
-        print("Nome capturado:", nome)
-    except Exception as e:
-        print(f"Erro ao buscar o nome na linha {row}: {str(e)}")
-
-    row +=1
 
 while True:
-    pass
+    
+    bloco_processado = False
+
+    for i in range(7):
+
+        cell_a = f"A{row}"
+        value_a = ler_celula(cell_a)
+
+        if not value_a or value_a.strip() == "":
+            print("Fim da planilha. Encerrando...")
+            exit()
+
+        cell_h = f"H{row}"
+        value_h = ler_celula(cell_h)
+
+        if value_h.strip().upper() == "PROCESSANDO":
+            try:
+                #CAPTURAR NOME PAINELFOMENTO
+                xpath_dinamico = f"//td[@class='text-left' and contains(text(), '{value_a}')]"
+                nome_td = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, xpath_dinamico)))
+                nome = nome_td.text
+
+                #Capturar Cartão, valor e beneficiário 
+                #.
+                #.
+                #.
+                
+                #CAPTURAR BOTÃO DE SELECIONAR
+                botaoselecionar = xpath_dinamico + "/preceding-sibling::td[1]//div[contains(@class, 'q-toggle')]"
+                botao1 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, botaoselecionar)))
+                #CLICAR NO BOTÃO
+                actions = ActionChains(driver)
+                actions.move_to_element(botao1).click().perform()
+
+                bloco_processado = True
+
+                cell_h = f"H{row}"
+                escrever_celula(cell_h, "LIBERADO")
+
+                print("Nome capturado:", nome)
+            except Exception as e:
+                print(f"Erro ao buscar o nome na linha {row}: {str(e)}")
+
+            row +=1
+
+    #Esperar para clicar em "Enviar Transferências Selecionadas"
+    time.sleep(3)
+
+    if bloco_processado:
+        try:
+            #ENVIAR TRANSFERÊNCIAS SELECIONADAS
+            botaotransferencias = "//span[contains(@class, 'q-btn__content') and span[text()='Enviar Transferências Selecionadas']]"
+            botao2 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, botaotransferencias)))
+            #CLICAR NO BOTÃO
+            actions = ActionChains(driver)
+            actions.move_to_element(botao2).click().perform()
+            print("Botão 'Enviar Transferências Selecionadas' clicado com sucesso!")
+
+
+            time.sleep(2)
+
+
+            #ENVIAR
+            botao_enviar = "//span[@class='block' and text()='Enviar']"
+            botao3 = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, botao_enviar)))
+            #CLICAR NO BOTÃO
+            actions = ActionChains(driver)
+            actions.move_to_element(botao_enviar).click().perform()
+
+        except Exception as e:
+            print(f"Erro ao clicar no botão de transferências: {str(e)}")
+
+
+    time.sleep(40)
+
+
