4a6523f0958c5099fe2b302a5d5f8ac07ca0ac5b
Guilherme Nicolao <guilhermenicolaodasilva45@gmail.com>
2025-05-20 17:41:10 -0300

    Reestruturado em forma de ERP o Orçamento Compras e Viabilidade Comercial - Elo e criado uma pasta Automações para as demais automações

--- GIMAVE/erp/app.py
+++ GIMAVE/erp/app.py
@@ -1,18 +1,18 @@
-from flask import Flask, render_template, request, redirect, flash, jsonify, url_for
-import os
-import re
-import mysql.connector
+from flask import Flask, render_template, request, redirect, flash, jsonify, url_for, session
+from collections import defaultdict
 from dotenv import load_dotenv
 from datetime import datetime
-from collections import defaultdict
 from decimal import Decimal
+from functools import wraps
+import mysql.connector
+import bcrypt
+import os
+import re
 
+# Load nas credenciais e no DB
 load_dotenv()
-
 app = Flask(__name__)
-app.secret_key = os.getenv("SK")  # Necessária pro flash funcionar
-
-#Configuração do DB
+app.secret_key = os.getenv("SK")  
 db_config = {
     "host": os.getenv("HOST"),
     "user": os.getenv("USER"),
@@ -21,14 +21,80 @@ db_config = {
 }
 
 
-# Homepage 
+#---------------LOGIN E TELAS INICIAIS----------------#
+# Login
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        senha = request.form['senha']
+
+        try:
+            conexao = mysql.connector.connect(**db_config)
+            cursor = conexao.cursor()
+            cursor.execute("SELECT senha_hash, nivel FROM usuarios_com WHERE username = %s", (username,))
+            resultado = cursor.fetchone()
+        finally:
+            cursor.close()
+            conexao.close()
+
+        if resultado:
+            senha_hash, nivel = resultado
+            if bcrypt.checkpw(senha.encode('utf-8'), senha_hash.encode('utf-8') if isinstance(senha_hash, str) else senha_hash):
+                session['username'] = username
+                session['nivel'] = nivel
+                flash('Login realizado com sucesso!', 'success')
+                return redirect(url_for('menu_principal'))
+
+        flash('Credenciais inválidas', 'danger')
+
+    return render_template('login.html')
+
+# Logout
+@app.route('/logout', methods=['POST'])
+def logout():
+    # Limpar a sessão para deslogar o usuário
+    session.clear()
+
+    # Adicionar uma mensagem de sucesso ao flash
+    flash('Sessão encerrada', 'success')
+
+    # Redirecionar para a página de login
+    return redirect(url_for('login'))
+
+#Tela inicial ERP
+@app.route('/system')
+def menu_principal():
+    if 'username' not in session:
+        flash('Você precisa estar logado para acessar esta página.', 'warning')
+        return redirect(url_for('login'))
+    return render_template('system.html')
+
+# Homepage | Orçamento Compras
 @app.route('/home')
-def home():
+def homecompras():
+    if 'username' not in session:
+        flash('Você precisa estar logado para acessar esta página.', 'warning')
+        return redirect(url_for('login'))
     return render_template('home_fin.html')
 
+# Homepage | Viabilidade Comercial - Elo
+@app.route('/home_com')
+def homecomercial():
+    if 'username' not in session:
+        flash('Você precisa estar logado para acessar esta página.', 'warning')
+        return redirect(url_for('login'))
+    return render_template('home_com.html')
+#-------------------------------------------------------#
+
+
+# ----------ROTAS ORÇAMENTO COMPRAS---------------------#
 #Rotas de cadastro de fornecedores
 @app.route('/fornecedores')
 def fornecedores_form():
+    if 'username' not in session:
+        flash('Você precisa estar logado para acessar esta página.', 'warning')
+        return redirect(url_for('login'))
     return render_template('fornecedores_fin.html')
 
 @app.route('/api/fornecedores')
@@ -136,10 +202,12 @@ def fornecedores_sugestoes():
     return jsonify(resultados)
 
 
-
 #Rotas Produtos
 @app.route('/produtos')
 def produtos_form():
+    if 'username' not in session:
+        flash('Você precisa estar logado para acessar esta página.', 'warning')
+        return redirect(url_for('login'))
     return render_template('produtos_fin.html')
 
 @app.route('/cadastrar_produto', methods=['POST'])
@@ -267,11 +335,12 @@ def produtos_sugestoes():
     return jsonify(resultados)
 
 
-
-
 #Rotas Categorias
 @app.route('/categorias')
 def categorias_form():
+    if 'username' not in session:
+        flash('Você precisa estar logado para acessar esta página.', 'warning')
+        return redirect(url_for('login'))
     return render_template('categoria_fin.html')
 
 @app.route('/cadastrar_categoria', methods=['POST'])
@@ -362,10 +431,12 @@ def categorias_sugestoes():
     return jsonify(resultados)
 
 
-
 #Rotas Orçamentos
 @app.route('/cadastro')
 def cadastro_form():
+    if 'username' not in session:
+        flash('Você precisa estar logado para acessar esta página.', 'warning')
+        return redirect(url_for('login'))
     return render_template('cadastro_fin.html')
 
 @app.route('/cadastrar', methods=['POST'])
@@ -618,6 +689,10 @@ def remover_orcamento(cod):
 # Rota Dashboard
 @app.route('/dashboard')
 def dashboard():
+    if 'username' not in session:
+        flash('Você precisa estar logado para acessar esta página.', 'warning')
+        return redirect(url_for('login'))
+    
     data_inicio = request.args.get('data_inicio')
     data_fim = request.args.get('data_fim')
     produto_id = request.args.get('produto_id')
@@ -712,7 +787,69 @@ def dashboard():
                            relatorio=relatorio, 
                            produtos=produtos, 
                            total_economia=float(total_economia))
+#-----------------------------------------------------------------#
 
+
+#----------ROTAS VIABILIDADE COMERCIAL ELO------------------------#
+# Parâmetros
+@app.route('/parametros')
+def parametros():
+    if 'username' not in session:
+        flash('Você precisa estar logado para acessar esta página.', 'warning')
+        return redirect(url_for('login'))
+
+    # Verifica se o usuário tem permissão de ADMIN
+    if session.get('nivel') != 'ADMIN':
+        flash('Você não tem permissão para acessar esta página.', 'danger')
+        return redirect(url_for('homecomercial'))  # Redireciona para a home comercial
+
+    return render_template('parametros_com.html')
+
+# Registrar-se
+@app.route('/registrar', methods=['GET', 'POST'])
+def registrar():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        username = request.form['username']
+        senha = request.form['senha']
+        nivel = request.form['nivel'].upper()
+
+        # Conectar ao banco usando db_config
+        conn = mysql.connector.connect(**db_config)  # Usando **db_config para passar os parâmetros de conexão
+        cursor = conn.cursor()
+
+        # Verificar se o usuário já existe
+        cursor.execute("SELECT COUNT(*) FROM usuarios_com WHERE username = %s", (username,))
+        result = cursor.fetchone()
+
+        # Se o usuário já existir, exibe mensagem e não tenta cadastrar
+        if result[0] > 0:
+            flash('Este usuário já existe!', 'danger')
+            cursor.close()
+            conn.close()
+            return render_template('registrar_com.html')
+
+        # Criptografa a senha
+        senha_hash = bcrypt.hashpw(senha.encode('utf-8'), bcrypt.gensalt())
+
+        try:
+            cursor.execute(
+                "INSERT INTO usuarios_com (nome, username, senha_hash, nivel) VALUES (%s, %s, %s, %s)",
+                (nome, username, senha_hash, nivel)
+            )
+            conn.commit()
+            flash('Usuário cadastrado com sucesso!', 'success')
+        except mysql.connector.Error as err:
+            flash(f'Erro ao cadastrar: {err}', 'danger')
+        finally:
+            cursor.close()
+            conn.close()
+
+        return redirect(url_for('registrar'))
+
+    return render_template('registrar_com.html')
+
+#------------------------------------------------------------------#
 
 
 if __name__ == '__main__':
